0088f35169f34b4f910869385a754125c5d6f74f
Pradeep <krpradeep891@gmail.com>
2022-08-26 20:26:25 +0530

    Updates

---
+++ precache-manifest.9102a6421b539049e9f978ff458c5012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1710f81de0edf981a186",
+    "url": "/My-Portfolio/static/css/main.d4a7af39.chunk.css"
+  },
+  {
+    "revision": "1710f81de0edf981a186",
+    "url": "/My-Portfolio/static/js/main.1710f81d.chunk.js"
+  },
+  {
+    "revision": "6c277eca0a7d23ef495f",
+    "url": "/My-Portfolio/static/js/1.6c277eca.chunk.js"
+  },
+  {
+    "revision": "aa9d0237064e9f092376",
+    "url": "/My-Portfolio/static/js/runtime~main.aa9d0237.js"
+  },
+  {
+    "revision": "de15f7e385ec0c433223f1fb905347d9",
+    "url": "/My-Portfolio/static/media/iiit-logo.de15f7e3.png"
+  },
+  {
+    "revision": "03ff59360022b410c4269e74be27e07b",
+    "url": "/My-Portfolio/static/media/profile2.03ff5936.jpg"
+  },
+  {
+    "revision": "74169150d7fb7563fa2d82c2b333fdb3",
+    "url": "/My-Portfolio/index.html"
+  }
+];
